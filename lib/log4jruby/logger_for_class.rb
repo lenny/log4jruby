27e592d1514e8dfb079079471d8289cf82bc5ccf
Lenny Marks <lenny@aps.org>
2010-11-14 00:34:16 -0500

    Rdoc for LoggerForClass

--- lib/log4jruby/logger_for_class.rb
+++ lib/log4jruby/logger_for_class.rb
@@ -1,6 +1,8 @@
 require 'log4jruby'
 
 module Log4jruby
+  
+  # Gives class its own Logger and makes it available via class and instance method. 
   module LoggerForClass
     
     def self.included(klass) # :nodoc:
