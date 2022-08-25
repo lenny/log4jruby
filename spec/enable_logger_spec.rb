28def7d911bd1ec9e4fe7c4e8c9fea185bc2a215
Lenny Marks <lenny@aps.org>
2022-08-25 19:38:29 -0400

    spec: Fix enable_logger_spec to work with new LogManager state

--- spec/enable_logger_spec.rb
+++ spec/enable_logger_spec.rb
@@ -4,24 +4,55 @@ require 'spec_helper'
 
 require 'log4jruby'
 
-module EnableLoggerSpec
-  class LogEnabledClass
-    enable_logger
+describe '.enable_logger injects a logger', log_capture: true do
+  prepend_before do
+    Log4jruby::Support::LogManager.reset
+  end
 
-    def echo(str)
-      logger.debug(str)
+  let(:klass) do
+    Class.new do
+      enable_logger
+
+      class << self
+        def name
+          'Klass'
+        end
+
+        def echo(str)
+          logger.debug(str)
+        end
+      end
+
+      def echo(str)
+        logger.debug(str)
+      end
     end
   end
-end
 
-describe '.enable_logger injects a logger', log_capture: true do
   specify 'lo4j logger is named for class' do
-    expect(EnableLoggerSpec::LogEnabledClass.logger.log4j_logger.name)
-      .to include('LogEnabledClass')
+    expect(klass.logger.log4j_logger.name)
+      .to include('Klass')
   end
 
-  specify 'logger is available to instance' do
-    EnableLoggerSpec::LogEnabledClass.new.echo('foo')
+  specify 'logger is available to class methods' do
+    klass.echo('foo')
     expect(log_capture).to include('foo')
+  end
+
+  specify 'logger is available to instance methods' do
+    # Java::org.apache.logging.log4j.LogManager.rootLogger.error("test")
+    # raise EnableLoggerSpec::LogEnabledClass.new.send(:logger).log4j_logger.getAppenders.inspect
+    # raise  Java::org.apache.logging.log4j.LogManager.rootLogger.level.toString
+    klass.new.echo('foo')
+    expect(log_capture).to include('foo')
+  end
+
+  specify 'subclasses get distinct loggers' do
+    subclass = Class.new(klass) do
+      def self.name
+        'SubClass'
+      end
+    end
+    expect(subclass.logger.name).to eq('jruby.SubClass')
   end
 end
