0b2958654ae0172cd27d0b71ab3bc3539fc37506
Lenny Marks <lenny@aps.org>
2022-08-25 15:20:03 -0400

    chrore: Updated log4j2 implementation for testing log_capture

--- spec/support/log_capture.rb
+++ spec/support/log_capture.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 shared_context 'log capture', log_capture: true do
-  let(:log_capture) { @log_stream.toString }
+  let(:log_stream) { Java::java.io.ByteArrayOutputStream.new }
+  let(:log_capture) { log_stream.toString }
 
   before do
-    @log_stream = Java::java.io.ByteArrayOutputStream.new
-    root_log4j_logger = Log4jruby::Logger.root.log4j_logger
-    root_log4j_logger.removeAllAppenders
-    layout = Java::org.apache.log4j.PatternLayout
-                 .new('%5p %.50X{fileName} %X{methodName}:%X{lineNumber} - %m%n')
-    appender = Java::org.apache.log4j.WriterAppender.new(layout, @log_stream)
-    appender.setImmediateFlush(true)
-    root_log4j_logger.addAppender(appender)
+    layout = Java::org.apache.logging.log4j.core.layout.PatternLayout.newBuilder
+                 .withPattern('%5p %.50X{fileName} %X{methodName}:%X{lineNumber} - %m%n').build
+    appender = Java::org.apache.logging.log4j.core.appender.OutputStreamAppender
+                        .newBuilder.setName('memory')
+                        .setImmediateFlush(true)
+                        .withLayout(layout).setTarget(log_stream).build
+    # log4j2 logger attributes are inherited from config only
+    # i.e. loggers will not inherit properties from private config on rootLogger
+    root_config = Java::org.apache.logging.log4j.LogManager.rootLogger.get
+    root_config.level = Java::org.apache.logging.log4j.Level::DEBUG
+    root_config.addAppender(appender, nil, nil)
   end
 end
